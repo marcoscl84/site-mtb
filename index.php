66b89ae40af95990dd099a3c7c32a04d9cdb2c09
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2021-09-12 23:06:19 -0300

    Inserção de banners

--- index.php
+++ index.php
@@ -7,12 +7,89 @@
 
     <link rel="stylesheet" href="main.css"/>
 
+    <script>
+        var slideIndex = 1;
+        showSlides(slideIndex);
+ 
+        // Next/previous controls
+        function plusSlides(n) {
+        showSlides(slideIndex += n);
+        }
+ 
+        // Thumbnail image controls
+        function currentSlide(n) {
+        showSlides(slideIndex = n);
+        }
+ 
+        function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += "active";
+        }
+ 
+        ///////
+
+        var slideIndex = 0;
+        showSlides();
+ 
+        function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 5000); // Change image every 5 seconds
+        }
+    </script>
+
     <title>Mundo das bikes</title>
 </head>
 <body>
     <?php include "header.php"; ?>
     <hr>
-    corpo do site
+    
+    <!-- Slideshow container -->
+    <div class="slideshow-container">
+
+        <!-- Full-width images with number and caption text -->
+        <div class="mySlides fade">
+            <img src="imagens/DSCN0747-2.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+            <img src="imagens/20210408_174856_HDR-2.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+            <img src="imagens/20200808_162240-2.jpg" style="width:100%">
+        </div>
+
+        <!-- Next and previous buttons -->
+        <a class="prev" onclick="plusSlides(-1)">❮</a>
+        <a class="next" onclick="plusSlides(1)">❯</a>
+    </div>
+    <br>
+
+    <!-- The dots/circles -->
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+
     <hr>
     <?php include "footer.php"; ?>
 </body>
