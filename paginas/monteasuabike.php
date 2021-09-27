5400ebf7c05652a47c0b8134ee42c6023062ece0
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2021-09-27 19:57:13 -0300

    Alteração do tipo de formulário na página de intereacao

--- paginas/monteasuabike.php
+++ paginas/monteasuabike.php
@@ -15,39 +15,56 @@ if(isset($_REQUEST['envia'])){
 
 <head>
     <link rel="stylesheet" href="../main.css"/>
+    <script>
+        function opcaoOculta(){
+            if(document.getElementById("mod").value == '1'){
+                document.getElementById("mod").style.display = 'flex';
+                document.getElementById("tip").style.display = 'flex';    
+            }
+        }
+    </script>
 </head>
 <body>
     <?php include "../header.php"; ?>
     <div class="container-wout-header">
         <br>
         <form method="$_REQUEST">
-            <div class="seleciona-modalidade">
-                <input name="rdModalidade" value="1" type="radio">XCO</input>
-                <input name="rdModalidade" value="2" type="radio">Enduro</input>
-                <input name="rdModalidade" value="3" type="radio">Downhill</input>
-                <input name="rdModalidade" value="4" type="radio">E-MTB</input>
-            </div>
+        <label>Selecione sua modalidade</label><br>
+            <select class="seleciona-modalidade" id="mod" onchange="opcaoOculta()">
+                <option></option>
+                <option name="rdModalidade" value="1">XCO</option>
+                <option name="rdModalidade" value="2">Enduro</option>
+                <option name="rdModalidade" value="3">Downhill</option>
+                <option name="rdModalidade" value="4">E-MTB</option>
+            </select>
             <br>
-            <div class="seleciona-marca">
-                <img src="<?php echo $url_base ?>/imagens/scott-sports-vector-logo.jpg" alt="Scott" style="width:100px"/>
-                <img src="<?php echo $url_base ?>/imagens/Specialized-logo-85B2A87B61-seeklogo.jpg" alt="SPZ" style="width:100px"/>
-                <img src="<?php echo $url_base ?>/imagens/cannondale-4c046f61-d1f7-4446-b2a6-d7fef877b85e.jpg" alt="Cannondale" style="width:100px"/>
-                <img src="<?php echo $url_base ?>/imagens/TREK-logo-A449338C0F-seeklogo.jpg" alt="Trek" style="width:100px"/>
-                <br>
-                <input name="rdMarca" value="1" type="radio">Scott</input>
-                <input name="rdMarca" value="2" type="radio">Specialized</input>
-                <input name="rdMarca" value="3" type="radio">Cannodale</input>
-                <input name="rdMarca" value="4" type="radio">Trek</input>
-            </div>
+            <label>Selecione a marca preferida</label><br>
+            <select class="seleciona-marca" id="marc">
+                            <!--
+                            <img src="<?php //echo $url_base ?>/imagens/scott-sports-vector-logo.jpg" alt="Scott" style="width:100px"/>
+                            <img src="<?php //echo $url_base ?>/imagens/Specialized-logo-85B2A87B61-seeklogo.jpg" alt="SPZ" style="width:100px"/>
+                            <img src="<?php //echo $url_base ?>/imagens/cannondale-4c046f61-d1f7-4446-b2a6-d7fef877b85e.jpg" alt="Cannondale" style="width:100px"/>
+                            <img src="<?php //echo $url_base ?>/imagens/TREK-logo-A449338C0F-seeklogo.jpg" alt="Trek" style="width:100px"/>
+                            <br>
+                            -->
+                <option></option>
+                <option name="rdMarca" value="1">Scott</option>
+                <option name="rdMarca" value="2">Specialized</option>
+                <option name="rdMarca" value="3">Cannodale</option>
+                <option name="rdMarca" value="4">Trek</option>
+            </select>
             <br>
-            <div class="seleciona-tipo">
-                <img src="<?php echo $url_base ?>/imagens/posters-bicycle-types-vector-illustration.jpg" alt="FullSuspension" style="width:100px"/>
-                <img src="<?php echo $url_base ?>/imagens/posters-bicycle-types-vector-illustration ht.jpg" alt="HardTail" style="width:100px"/>
-                <br>
-                <input name="rdTipo" value="1" type="radio">Full Suspension</input>
-                <input name="rdTipo" value="2" type="radio">Hard tail</input>
-            </div>
-
+            <select class="seleciona-tipo" id="tip" style="display:none;" onchange="opcaoOculta()">
+                            <!--
+                            <img src="<?php //echo $url_base ?>/imagens/posters-bicycle-types-vector-illustration.jpg" alt="FullSuspension" style="width:100px"/>
+                            <img src="<?php //echo $url_base ?>/imagens/posters-bicycle-types-vector-illustration ht.jpg" alt="HardTail" style="width:100px"/>
+                            <br>
+                            -->
+                <label>Selecione o tipo de amortecimento</label>
+                <option name="rdTipo" value="1">Full Suspension</option>
+                <option name="rdTipo" value="2">Hard Tail</option>
+            </select>
+            
             <input type="submit" id="envia" value="Enviar"/>            
         </form>
         
