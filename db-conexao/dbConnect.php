7c4b1262b22d18cae2717a05c8938791ac5fab8f
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2021-10-06 17:09:45 -0300

    correções no formulário e ajuste no código php para pesquisa no banco

--- db-conexao/dbConnect.php
+++ db-conexao/dbConnect.php
@@ -4,4 +4,7 @@ $conexao = mysqli_connect('localhost','root','');
 $banco = mysqli_select_db($conexao,'apresenta-bike');
 mysqli_set_charset($conexao,'utf8');
 
+
+
 ?>
+
--- paginas/monteasuabike.php
+++ paginas/monteasuabike.php
@@ -5,11 +5,19 @@ if(isset($_REQUEST['envia'])){
     $rdMod = $_REQUEST['rdModalidade'];
     $rdMarca = $_REQUEST['rdMarca'];
     $rdTipo = $_REQUEST['rdTipo'];
+
+    if($rdTipo == 0){
+        $rdTipo = 1;
+    } 
+
+    include "../db-conexao/dbConnect.php";
  
-    require("dbConnect.php");
- 
-    $bikeSelectNoBanco = mysqli_query($conexao, "SELECT * FROM 'bike' WHERE 
+    $bikeSelectNoBanco = mysqli_query($conexao, "SELECT foto FROM bike WHERE 
     id_modalidade = $rdMod AND id_marca = $rdMarca AND id_tipo = $rdTipo");
+
+    while($bikeShow = mysqli_fetch_assoc($bikeSelectNoBanco)){
+        $bke = "http://localhost/apresenta-bikes/".$bikeShow['foto'];
+    }
 } 
 ?>
 
@@ -33,17 +41,17 @@ if(isset($_REQUEST['envia'])){
     <div class="container-wout-header">
         <br>
         
-        <form method="$_REQUEST">
+        <form method="post" action="monteasuabike.php">
             <div class="seleciona-itens">
-                <select class="seleciona-modalidade" id="mod" onchange="opcaoOculta()">
+                <select class="seleciona-modalidade" name="rdModalidade" id="mod" onchange="opcaoOculta()">
                     <option>MODALIDADE:</option>
-                    <option name="rdModalidade" value="1">XCO</option>
-                    <option name="rdModalidade" value="2">Enduro</option>
-                    <option name="rdModalidade" value="3">Downhill</option>
-                    <option name="rdModalidade" value="4">E-MTB</option>
+                    <option value="1">XCO</option>
+                    <option value="2">Enduro</option>
+                    <option value="3">Downhill</option>
+                    <option value="4">E-MTB</option>
                 </select>
 
-                <select class="seleciona-marca" id="marc">
+                <select class="seleciona-marca" name="rdMarca" id="marc">
                                 <!--
                                 <img src="<?php //echo $url_base ?>/imagens/scott-sports-vector-logo.jpg" alt="Scott" style="width:100px"/>
                                 <img src="<?php //echo $url_base ?>/imagens/Specialized-logo-85B2A87B61-seeklogo.jpg" alt="SPZ" style="width:100px"/>
@@ -52,36 +60,38 @@ if(isset($_REQUEST['envia'])){
                                 <br>
                                 -->
                     <option>MARCA:</option>
-                    <option name="rdMarca" value="1">Scott</option>
-                    <option name="rdMarca" value="2">Specialized</option>
-                    <option name="rdMarca" value="3">Cannondale</option>
-                    <option name="rdMarca" value="4">Trek</option>
+                    <option value="1">Scott</option>
+                    <option value="2">Specialized</option>
+                    <option value="3">Cannondale</option>
+                    <option value="4">Trek</option>
                 </select>
 
-                <select class="seleciona-tipo" id="tip" style="display:none;" onchange="opcaoOculta()">
+                <select class="seleciona-tipo" name="rdTipo"  id="tip" style="display:none;" onchange="opcaoOculta()">
                                 <!--
                                 <img src="<?php //echo $url_base ?>/imagens/posters-bicycle-types-vector-illustration.jpg" alt="FullSuspension" style="width:100px"/>
                                 <img src="<?php //echo $url_base ?>/imagens/posters-bicycle-types-vector-illustration ht.jpg" alt="HardTail" style="width:100px"/>
                                 <br>
                                 -->
-                    <option>TIPO:</option>
-                    <option name="rdTipo" value="1">Full Suspension</option>
-                    <option name="rdTipo" value="2">Hard Tail</option>                    
+                    <option value="0">TIPO:</option>
+                    <option value="1">Full Suspension</option>
+                    <option value="2">Hard Tail</option>                    
                 </select>                       
             </div>    
             
             <div class="submit-botao">
-                <input type="submit" id="envia" value="Enviar"/>
+                <input type="submit" name="envia" id="envia" value="Enviar"/>
             </div>   
 
         </form>
         
         <!-- Exibe dados da busca no banco -->
-        <?php 
-            if(isset($bikeSelectNoBanco)){
-                echo $bikeSelectNoBanco;
-            }
-        ?>
+        <div class="monte-a-sua">
+            <?php 
+                if(isset($bke)){
+                    ?> <img id="imageMonteSua" src="<?php echo $bke ?>" style="max-height:600px;"/> <?php
+                }
+            ?>
+        </div>
 
         <br>
         <?php include "../footer.php"; ?>
