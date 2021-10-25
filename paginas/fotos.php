4aa034a77ab2af9f8cf6491df394c9624443a397
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2021-10-24 23:11:33 -0300

    estilização e inserção de conteúdo

--- paginas/fotos.php
+++ paginas/fotos.php
@@ -7,15 +7,37 @@
     </div>
 
     <div class="container-wout-header">
-        <br>
         
         <div class="thumbnail">
             <div class="fotos">
-                <img src="#" alt="imagem" />
+                <img src="../imagens/henrique-avancini-termina-mountain-bike-em-13-lugar-566188-article.jpg" alt="imagem" />
             </div>
-            <br>
             <div class="fotos">
-                <img src="#" alt="imagem" />
+                <img src="../imagens/dh-vallnord-world-cup-qualifying-loic-bruni.jpg" alt="imagem" />
+            </div>
+            <div class="fotos">
+                <img src="../imagens/s1600_Untitled_1_404901.jpg" alt="imagem" />
+            </div>
+            <div class="fotos">
+                <img src="../imagens/s1600_EWS_Les_Orres_2019EWS_Les_Orres_2019_W0A5574_557024.jpg" alt="imagem" />
+            </div>
+            <div class="fotos">
+                <img src="../imagens/Enduro-World-Series-2019-in-Zermatt_front_magnific.jpg" alt="imagem" />
+            </div>
+            <div class="fotos">
+                <img src="../imagens/brasil_olimpiadas_1024x576_26072021053909.jpg" alt="imagem" />
+            </div>
+            <div class="fotos">
+                <img src="../imagens/are-you-ready-to-rampage.jpg" alt="imagem" />
+            </div>
+            <div class="fotos">
+                <img src="../imagens/20190622_160001 (1).jpg" alt="imagem" />
+            </div>
+            <div class="fotos">
+                <img src="../imagens/11025002-w1920.jpg" alt="imagem" />
+            </div>
+            <div class="fotos">
+                <img src="../imagens/64a8a20be4.jpg" alt="imagem" />
             </div>
         </div>
         
