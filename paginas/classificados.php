3e88c60724465ff735875e694f416dc6041fa46a
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2021-12-05 00:06:15 -0300

    exclusão de código não utilizado

--- paginas/classificados.php
+++ paginas/classificados.php
@@ -7,43 +7,6 @@
 </head>
  
 <body>
-
-    <!-- SCRIPT MODAL -->
-    <script>
-        funcaoExcluir(){
-            var myModal = document.getElementById('janelaModal');
-            var myInput = document.getElementById('deleteButton');
-
-            myModal.addEventListener('shown.bs.modal', function () {
-                myInput.focus();
-            })
-        }
-
-    </script>
-
-    <!----- MODAL EXCLUIR ------->
-    <div class="modal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Título do modal</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                
-                <div class="modal-body">
-                    <p>Texto do corpo do modal, é aqui.</p>
-                </div>
-                
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                    <button type="button" class="btn btn-primary">Salvar mudanças</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="cabecalho">
         <?php include "../header.php"; ?>
     </div>
